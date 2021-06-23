c8df1f09c27a1913c02165f653dfa49deafff124
Patricio <patoghillino@gmail.com>
2021-06-22 23:08:30 -0300

    first commit

---
+++ src/test/java/com/cash/exam/CashExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cash.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
